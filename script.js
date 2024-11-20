a19535b1054d83b0bc1079c55a21248ff5b95c07
jeremycoolgamer <115514831+mrfoxybro@users.noreply.github.com>
2024-11-19 19:48:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 
-$("head").ready(function(){
-	$("a").click(function(){
-		$("*").css("cursor","wait");
-		$("#click")[0].play();
-	}); 
-});
-$(window).bind("pageshow", function(event) {
-    if (event.originalEvent.persisted) {
-        //$("*").css("cursor","auto");
-		$("*").css("cursor","default");
-    }
-});
